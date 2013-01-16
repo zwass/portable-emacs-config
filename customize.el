a231620c190667c42686920488337819ffeec65b
Zachary Wasserman <zachwass2000@gmail.com>
2013-01-16 17:36:08 -0500

    Various customize (tex, javascript, window-numbering)

--- customize.el
+++ customize.el
@@ -4,7 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(js-indent-level 2))
+ '(TeX-newline-function (quote reindent-then-newline-and-indent))
+ '(js-indent-level 2)
+ '(window-numbering-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
